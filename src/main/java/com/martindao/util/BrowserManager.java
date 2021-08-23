764db4a8e673b2e7d9a595c826154d750f43192c
martindao <35680899+martindao@users.noreply.github.com>
2021-08-23 10:19:00 -0600

    Tighten Allure reporting around Reporting

--- src/main/java/com/martindao/util/BrowserManager.java
+++ src/main/java/com/martindao/util/BrowserManager.java
@@ -20,3 +20,5 @@ public final class BrowserManager {
 }
 
 # [2021-08-16] (Checkout) schedule note: Refactor fixtures for Checkout
+
+# [2021-08-23] (Reporting) schedule note: Tighten Allure reporting around Reporting

64169a0e99ea468fa745afa4ed3b8c0a59ec3917
martindao <35680899+martindao@users.noreply.github.com>
2021-09-15 12:51:00 -0600

    Document Playwright coverage for Catalog

--- src/main/java/com/martindao/ui/component/BaseComponent.java
+++ src/main/java/com/martindao/ui/component/BaseComponent.java
@@ -19,3 +19,5 @@ public abstract class BaseComponent {
 # [2021-08-06] (Checkout) schedule note: Document Playwright coverage for Checkout
 
 # [2021-08-17] (Reporting) schedule note: Document Playwright coverage for Reporting
+
+# [2021-09-15] (Catalog) schedule note: Document Playwright coverage for Catalog

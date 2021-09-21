6cac67f775415198118465428b2a215b1ebec283
martindao <35680899+martindao@users.noreply.github.com>
2021-09-21 11:11:00 -0600

    Add Playwright regression scenario for Reporting

--- src/test/java/com/martindao/annotation/Regression.java
+++ src/test/java/com/martindao/annotation/Regression.java
@@ -16,3 +16,5 @@ public @interface Regression {}
 # [2021-08-19] (Reporting) schedule note: Document Playwright coverage for Reporting
 
 # [2021-08-24] (Catalog) schedule note: Document Playwright coverage for Catalog
+
+# [2021-09-21] (Reporting) schedule note: Add Playwright regression scenario for Reporting

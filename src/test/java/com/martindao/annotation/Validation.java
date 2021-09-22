502606a2a559afc902514544b0bf5ec5f8f33e97
martindao <35680899+martindao@users.noreply.github.com>
2021-09-22 14:45:00 -0600

    Document Playwright coverage for Checkout

--- src/test/java/com/martindao/annotation/Validation.java
+++ src/test/java/com/martindao/annotation/Validation.java
@@ -19,3 +19,5 @@ public @interface Validation {}
 # [2021-08-24] (Login) schedule note: Add Playwright regression scenario for Login
 
 # [2021-08-25] (Login) schedule note: Add Playwright regression scenario for Login
+
+# [2021-09-22] (Checkout) schedule note: Document Playwright coverage for Checkout

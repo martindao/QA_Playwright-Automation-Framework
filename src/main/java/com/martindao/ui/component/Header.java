4b97abf1a062b77fbc6552c5ee9b8c4dde9cc55d
martindao <35680899+martindao@users.noreply.github.com>
2021-10-08 12:27:00 -0600

    Document Playwright coverage for Catalog

--- src/main/java/com/martindao/ui/component/Header.java
+++ src/main/java/com/martindao/ui/component/Header.java
@@ -21,3 +21,5 @@ public final class Header extends BaseComponent {
 # [2021-08-09] (Login) schedule note: Tighten Allure reporting around Login
 
 # [2021-09-15] (Catalog) schedule note: Document Playwright coverage for Catalog
+
+# [2021-10-08] (Catalog) schedule note: Document Playwright coverage for Catalog

a2d8a3dfef63babc12a240db507e77ab618ab873
martindao <35680899+martindao@users.noreply.github.com>
2022-06-10 10:12:00 -0600

    Tighten Allure reporting around Catalog

--- src/test/java/com/martindao/util/TestDataArgumentsProvider.java
+++ src/test/java/com/martindao/util/TestDataArgumentsProvider.java
@@ -44,3 +44,5 @@ public class TestDataArgumentsProvider implements ArgumentsProvider, AnnotationC
 # [2021-11-23] (Checkout) schedule note: Document Playwright coverage for Checkout
 
 # [2021-12-23] (Catalog) schedule note: Refactor fixtures for Catalog
+
+# [2022-06-10] (Catalog) schedule note: Tighten Allure reporting around Catalog

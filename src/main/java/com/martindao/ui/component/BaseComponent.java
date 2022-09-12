e3599b6d961391e51712574f31aab030ce8a0ad6
martindao <35680899+martindao@users.noreply.github.com>
2022-09-12 12:14:00 -0600

    Document Playwright coverage for Login

--- src/main/java/com/martindao/ui/component/BaseComponent.java
+++ src/main/java/com/martindao/ui/component/BaseComponent.java
@@ -29,3 +29,5 @@ public abstract class BaseComponent {
 # [2021-12-14] (Checkout) schedule note: Document Playwright coverage for Checkout
 
 # [2022-03-29] (Reporting) schedule note: Refactor fixtures for Reporting
+
+# [2022-09-12] (Login) schedule note: Document Playwright coverage for Login

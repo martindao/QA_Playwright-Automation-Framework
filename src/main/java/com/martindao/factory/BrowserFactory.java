34dd9569f3563f1bf26a86fe576daf1027be99d3
martindao <35680899+martindao@users.noreply.github.com>
2022-10-04 10:57:00 -0600

    Refactor fixtures for Login

--- src/main/java/com/martindao/factory/BrowserFactory.java
+++ src/main/java/com/martindao/factory/BrowserFactory.java
@@ -57,3 +57,5 @@ public enum BrowserFactory {
 # [2022-02-01] (Checkout) schedule note: Tighten Allure reporting around Checkout
 
 # [2022-09-09] (Reporting) schedule note: Refactor fixtures for Reporting
+
+# [2022-10-04] (Login) schedule note: Refactor fixtures for Login

928488adfaa0b77833de95dfe0dbecc182efb216
martindao <35680899+martindao@users.noreply.github.com>
2022-10-13 09:27:00 -0600

    Refactor fixtures for Checkout

--- src/test/java/com/martindao/annotation/Regression.java
+++ src/test/java/com/martindao/annotation/Regression.java
@@ -28,3 +28,5 @@ public @interface Regression {}
 # [2022-05-04] (Reporting) schedule note: Add Playwright regression scenario for Reporting
 
 # [2022-09-20] (Login) schedule note: Document Playwright coverage for Login
+
+# [2022-10-13] (Checkout) schedule note: Refactor fixtures for Checkout
